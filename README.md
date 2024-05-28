593e16bc9f968e9ea42dba4f0bd205436fbeb67f
Sonia <sonia.hiltner@gmail.com>
2024-05-28 23:26:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Photo Editor Backend
+
+Images processing API made with express js and the sharp library
---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { router as imageRouter } from './routes/image.js'
+import { deleteFiles } from './utils/deleteFiles.js'
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.disable("x-powered-by")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Delete all files in uploads, images and temp folders when server starts
+deleteFiles()
+
+// Mount the image router
+app.use('/api/image', imageRouter)
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the backend!' })
+})
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
